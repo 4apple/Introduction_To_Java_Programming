f8c36c4ee16f1d7dbeac2bebaab797a9f70ddc77
macintosh <macintosh@macintoshdeMacBook-Pro.local>
2017-06-15 23:25:35 +0800

    submit compute area demo list

---
+++ ComputeArea/app/src/main/java/com/mango/javaprogramming/computearea/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mango.javaprogramming.computearea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tvResult);
+        tv.setText(String.valueOf(computeArea()));
+    }
+
+    double computeArea(){
+        double radius;
+        double area;
+        final double PI = 3.1415926;
+
+        radius = 20;
+
+        area = radius * radius * PI;
+
+        return area;
+    }
+}
